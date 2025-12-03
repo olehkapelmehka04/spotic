768b3aff4097ae651510104a5c29b103cc2ad2ff
Vanya <kn0w.reedz.77@mail.ru>
2025-12-03 20:05:37 +0300

    added urls

--- spotic/music/urls.py
+++ spotic/music/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path("playlist/", views.CreatePlaylistAPIView.as_view()),
     path("<int:song_id>/", views.SongAPIView.as_view()),
     path("playlist/<int:playlist_id>/", views.PlaylistAPIView.as_view()),
+    path("songs-by-genre/", views.FindByGenreAPIView.as_View()),
+    path("rate/<int:song_id>/", views.RateSongAPIView.as_view()),
+    path("my-wave/", views.MyWaveAPIView.as_view()),
 ]
